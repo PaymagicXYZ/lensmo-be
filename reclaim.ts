994aac5db631279470d6f912339176d4aca1eabf
Rory Hellyer <rory@webmodularity.com>
2022-09-19 22:03:45 -0700

    finish reclaim + add refill

--- reclaim.ts
+++ reclaim.ts
@@ -23,23 +23,115 @@ const chains: Chain[] = [
 async function main() {
     const chain = chains[0] // matic
     const pendingWallets = await getPendingWallets()
+    let walletsReclaimed = 0
+    let walletsAssigned = 0
     for (let i = 0;i < pendingWallets.length;i++) {
-        const walletAddress = pendingWallets[i].receive_wallet_id.wallet_address
+        const escrowUserId = pendingWallets[i].id
+        const walletAddress = pendingWallets[i].receive_wallets.wallet_address
         const socialUser = pendingWallets[i].user_id
         // Checks if is any current native, erc20, or erc721 balance
         const walletEmpty = await walletIsEmpty(chain, walletAddress)
         console.log(`${i + 1}: ${walletAddress} is empty: ${walletEmpty}`)
         if (walletEmpty) {
-            // Delete escrow_users row
-            // Delete receive_wallet row
-            // Re-insert receive_wallets.chain and receive_wallets.wallet_address
+            const removeUserResponse = await removeEscrowUser(escrowUserId) // status: 204
+            const removeWalletResponse = await removeReceiveWallet(chain, walletAddress) // status: 204
+            const insertWalletResponse = await insertReceiveWallet(chain, walletAddress) // status: 201
+            walletsReclaimed++
         } else {
-            // Update receive_wallet.status=’assigned’ and receive_wallet.updated_at = now()
+            const updateWalletResponse = await updateReceiveWallet(chain, walletAddress) // status: 200
             // Mint SBT
-            //const mintTxHash = mintToken(chain, socialUser, walletAddress)
-            //console.log("Mint transaction hash: ", mintTxHash)
+            const mintTxHash = await mintToken(chain, socialUser, walletAddress)
+            console.log("Mint transaction hash: ", mintTxHash)
+            walletsAssigned++
         }
     }
+    console.log("Wallets reclaimed: ", walletsReclaimed)
+    console.log("Wallets assigned: ", walletsAssigned)
+}
+
+const removeEscrowUser = async(id: string) => {
+    const config = {
+        headers: {
+            "apiKey": process.env['DATA_API_KEY'] as string,
+            "Authorization": 'Bearer ' + process.env['DATA_API_KEY'] as string
+        },
+        params: {
+            "id": 'eq.' + id
+        },
+        baseURL: supabaseBaseUrl
+    }
+
+    return await axios.delete(
+        "escrow_users",
+        config
+    )
+}
+
+const removeReceiveWallet = async(chain: Chain, walletAddress: string) => {
+    const config = {
+        headers: {
+            "apiKey": process.env['DATA_API_KEY'] as string,
+            "Authorization": 'Bearer ' + process.env['DATA_API_KEY'] as string
+        },
+        params: {
+            "chain": 'eq.' + chain.name,
+            "wallet_address": 'eq.' + walletAddress
+        },
+        baseURL: supabaseBaseUrl
+    }
+
+    return await axios.delete(
+        "receive_wallets",
+        config
+    )
+}
+
+const insertReceiveWallet = async(chain: Chain, walletAddress: string) => {
+    const config = {
+        headers: {
+            "apiKey": process.env['DATA_API_KEY'] as string,
+            "Authorization": 'Bearer ' + process.env['DATA_API_KEY'] as string,
+            "Content-Type": "application/json",
+            "Prefer": "return=representation"
+        },
+        baseURL: supabaseBaseUrl
+    }
+
+    return await axios.post(
+        "receive_wallets",
+        {
+            "chain": chain.name,
+            "wallet_address": walletAddress,
+            "wallet_type": "safe",
+            "status": "pending"
+        },
+        config
+    )
+}
+
+const updateReceiveWallet = async(chain: Chain, walletAddress: string) => {
+    const config = {
+        headers: {
+            "apiKey": process.env['DATA_API_KEY'] as string,
+            "Authorization": 'Bearer ' + process.env['DATA_API_KEY'] as string,
+            "Content-Type": "application/json",
+            "Prefer": "return=representation"
+        },
+        params: {
+            "chain": 'eq.' + chain.name,
+            "wallet_address": 'eq.' + walletAddress
+        },
+        baseURL: supabaseBaseUrl
+    }
+
+    return await axios.patch(
+        "receive_wallets",
+        {
+            "status": "assigned",
+            "updated_at": new Date().toISOString()
+        },
+        config
+    )
 }
 
 const getPendingWallets = async() => {
@@ -47,12 +139,13 @@ const getPendingWallets = async() => {
 
     const config = {
         params: {
-            "select": 'id,user_id,created_at,updated_at,receive_wallet_id(chain,wallet_address,wallet_type,status,created_at,updated_at)',
-            "receive_wallet_id.status": 'eq.pending',
+            "select": 'id,user_id,created_at,updated_at,receive_wallets!inner(chain,wallet_address,wallet_type,status,created_at,updated_at)',
+            "receive_wallets.status": 'eq.pending',
             "updated_at": `lt.${bufferUpdatedTime}`
         },
         headers: {
-            "apiKey": process.env['DATA_API_KEY'] as string
+            "apiKey": process.env['DATA_API_KEY'] as string,
+            "Authorization": 'Bearer ' + process.env['DATA_API_KEY'] as string
         },
         baseURL: supabaseBaseUrl
     }
@@ -92,10 +185,31 @@ const mintToken = async(chain: Chain, socialUser: string, walletAddress: string)
     const provider = await getProvider(chain)
     const signer = new ethers.Wallet(process.env['SIGNER_PK']!, provider)
     const mintContract = new ethers.Contract(contractAddress, sbtContractAbi, signer)
-    const mintTx = await mintContract.mint(walletAddress, socialUser)
+    const gasOptions = await getGasOptions(chain, provider)
+    const mintTx = await mintContract.mint(walletAddress, socialUser, gasOptions)
     // Return here if we don't want to wait for tx to mine
     const receipt = await mintTx.wait()
     return receipt.transactionHash
+}
+
+const getGasOptions = async(chain: Chain, provider: providers.Provider): Promise<Object> => {
+    if (chain.name === "matic") {
+        const {data} = await axios({
+            method: 'get',
+            url: 'https://gasstation-mainnet.matic.network/v2'
+        })
+        return {
+            "maxFeePerGas": ethers.utils.parseUnits(Math.ceil(data.fast.maxFee).toString(), 'gwei'),
+            "maxPriorityFeePerGas": ethers.utils.parseUnits(Math.ceil(data.fast.maxPriorityFee).toString(), 'gwei')
+        }
+    } else {
+        const feeData = await provider.getFeeData()
+        const maxPriority = feeData.maxPriorityFeePerGas
+        return {
+            "maxPriorityFeePerGas": maxPriority,
+            "maxFeePerGas": feeData.maxFeePerGas!.add(maxPriority!)
+        }
+    }
 }
 
 const getProvider = async(chain: Chain): Promise<providers.Provider> => {
