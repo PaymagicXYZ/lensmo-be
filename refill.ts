856392de5e8784cc17849babda820bd18b65e4f8
Rory Hellyer <rory@webmodularity.com>
2022-09-21 22:20:31 -0700

    remove extra safe owner

--- refill.ts
+++ refill.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import * as dotenv from 'dotenv'
+import {ethers} from "ethers";
 dotenv.config()
 
 const targetWalletCount = 100;
@@ -12,33 +13,40 @@ async function main() {
     const chain = "matic"
     // Total escrow wallets
     const totalWalletCount = await getTotalReceiveWalletCount()
-    console.log("Total receive wallets: ", totalWalletCount)
+    console.log("Total n00b wallets: ", totalWalletCount)
 
     // Claimed escrow wallets
     const claimedWalletCount = await getClaimedReceiveWalletCount()
-    console.log("Claimed receive wallets: ", claimedWalletCount)
+    console.log("Claimed n00b wallets: ", claimedWalletCount)
 
     const availableWalletCount = totalWalletCount - claimedWalletCount
     const newWalletsNeeded = targetWalletCount - availableWalletCount
-    console.log("New receive wallets needed: ", newWalletsNeeded)
+    console.log("New n00b wallets needed: ", newWalletsNeeded)
 
     let newSafesAdded = 0;
     for (let i = 0;i < newWalletsNeeded;i++) {
         const newSafeAddress = await createEscrowWallet(chain)
         if (newSafeAddress) {
-            console.log(`New escrow safe created on ${chain} with address: ${newSafeAddress}`)
+            console.log(`New safe created on ${chain} with address: ${newSafeAddress}`)
             const addResponse = await addReceiveWallet(chain, newSafeAddress)
             if (addResponse.status === 201) {
                 newSafesAdded++
                 console.log("Safe added!")
+                // Remove extra owner
+                const removeTxHash = await removeExtraOwner(chain, newSafeAddress)
+                if (removeTxHash) {
+                    console.log("Removed extra safe owner")
+                } else {
+                    console.log("Failed to remove extra safe owner")
+                }
             } else {
                 console.log("Failed to add safe!")
             }
         } else {
-            console.log("Failed to create escrow safe... moving on")
+            console.log("Failed to create safe... moving on")
         }
     }
-    console.log("New escrow wallets added: ", newSafesAdded)
+    console.log("New n00b wallets added: ", newSafesAdded)
 }
 
 const getTotalReceiveWalletCount = async(): Promise<number> => {
@@ -126,6 +134,43 @@ const createEscrowWallet = async (chain: string): Promise<string | undefined> =>
     return undefined
 }
 
+const removeExtraOwner = async(chain: string, safeAddress: string): Promise<string | undefined> => {
+    const prevOwnerAddress = "0x0000000000000000000000000000000000000001"
+    const iface = new ethers.utils.Interface(safeAbi)
+    const removeOwnerTx = iface.encodeFunctionData("removeOwner", [prevOwnerAddress, walletExtraSignerAddress, 1])
+    return await removeOwnerTransaction(chain, safeAddress, removeOwnerTx)
+}
+
+const removeOwnerTransaction = async (chain: string, safeAddress: string, data: string): Promise<string | undefined> => {
+    const paymagicUrl = `${paymagicBaseUrl}/${chain}/account/${safeAddress}/custom`;
+    const response = await axios.post(paymagicUrl, {
+        "to": safeAddress,
+        "value": "0",
+        "operation": 0,
+        "data": data
+    }, {
+        headers: {
+            "x-api-key": process.env['PAYMAGIC_API_KEY'] as string
+        }
+    });
+    if (response.status === 200) {
+        const paymagicTxId = response.data.id;
+        let attempts = 0;
+        while (true) {
+            attempts++;
+            const txResponse = await paymagicTransactionStatus(chain, paymagicTxId);
+            const status = txResponse.status;
+            if (status === 'COMPLETE') {
+                return txResponse.currentHash;
+            }
+            else if (status === 'FAILED' || attempts >= 30) {
+                return undefined;
+            }
+            await delay(1000);
+        }
+    }
+};
+
 const paymagicTransactionStatus = async (chain: string, txId: string): Promise<any> => {
     const url = `${paymagicBaseUrl}/${chain}/tx/${txId}`
 
@@ -144,6 +189,8 @@ const paymagicTransactionStatus = async (chain: string, txId: string): Promise<a
 const delay = async (ms: number) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+const safeAbi = [{"inputs":[{"internalType":"address","name":"prevOwner","type":"address"},{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"_threshold","type":"uint256"}],"name":"removeOwner","outputs":[],"stateMutability":"nonpayable","type":"function"}]
 
 main()
     .then(() => process.exit(0))
